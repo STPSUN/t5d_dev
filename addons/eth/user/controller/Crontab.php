8ab979e054b762c1291ecfc859f9852136d7f67d
STPSUN <497432519@qq.com>
2019-01-04 10:36:14 +0800

    1

--- addons/eth/user/controller/Crontab.php
+++ addons/eth/user/controller/Crontab.php
@@ -17,7 +17,7 @@ class Crontab extends \web\common\controller\BaseController {
         $m = new \addons\eth\model\EthTradingOrder();
         $paramM = new \web\common\model\sys\SysParameterModel();
         $sys_address = $paramM->getValByName('out_address');
-        echo $sys_address;exit();
+//        echo $sys_address;exit();
         $filter = "address is not null";
         $list = $m->getRechargeByCoin(1);
 
